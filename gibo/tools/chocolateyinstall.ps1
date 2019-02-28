9dcb182684d95fe7c0c97bce35749cf89ff7d955
jeremy <jeremy@noemi.io>
2019-02-28 15:56:29 +0100

    gibo package

---
+++ gibo/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    fileFullPath  = Join-Path $toolsDir "gibo.bat"
+    url           = 'https://raw.githubusercontent.com/simonwhitaker/gibo/master/gibo.bat'
+    checksum      = 'b5b7776239add5d40c689b977464379012be16e2de2f0f66f824a2f4660e24c4'
+    checksumType  = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Install-BinFile -Name $env:ChocolateyPackageName -Path $packageArgs.fileFullPath
